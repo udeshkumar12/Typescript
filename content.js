234ca8fea8f27756dc559899e27685aa88773a3c
udeshkumar12 <udeshk949@gmail.com>
2026-04-04 02:57:25 -0400

    typescript

---
+++ content.js
@@ -0,0 +1,15 @@
+What is TypeScript?
+Why use TypeScript ?
+TypeScript setup
+Type Annotations
+Tuples
+Types in Objects
+Optional Property
+Interfaces
+Union and Intersection type
+type alias
+never type
+Class in typescript - public, private and protected
+implements in typescript
+Generics
+any type 
